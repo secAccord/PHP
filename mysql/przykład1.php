f541b64a07fe1f7c4217600e24e0ab7ba27b2e5c
Dawid 'Accord' Zgoda <unga.pl123@gmail.com>
2022-05-07 17:47:40 +0200

    y

--- mysql/przykład1.php
+++ mysql/przykład1.php
@@ -12,13 +12,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title><?php echo 'Znaleziono '.mysqli_num_rows($query)." kobiet" ?></title>
 </head>
 <body>
 <?php
     while($row = mysqli_fetch_row($query)){ //Wyciąganie wierszy do zmiennej do póki wierszę się nie kończą
         echo<<<END
-            $row[0] - $row[1] $row[2] <br />
+           <div> $row[0] - $row[1] $row[2] <div>
         END;
     }
 ?>
